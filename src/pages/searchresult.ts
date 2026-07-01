4c542b9ad4b0439bc2cc5e73b9665157d51dc022
vishal khokar <56104143+vishalkhokar@users.noreply.github.com>
2026-07-01 16:25:39 +0530

    update the Loc

--- src/pages/searchresult.ts
+++ src/pages/searchresult.ts
@@ -2,10 +2,11 @@ import { Locator, Page } from "@playwright/test";
 import { BasePage } from "./BasePage";
 export class SearchPage extends BasePage{
     private readonly searchResults:Locator;
-
+    private readonly xyLoc:Locator;
     constructor(page:Page){
         super(page);
         this.searchResults=page.locator('div.product-layout')
+        this.xyLoc=page.getByRole('button',{name:'xxx'})
     }
 
     async getProductSearchresultCount():Promise<number>{
